3cf129b5bb515cb2c95df3a0373cda8b0babbddf
Isuewo <g0vr26u7@duck.com>
2023-02-26 00:35:56 +0100

    mod

---
+++ src/main/java/de/isuewo/overimagined_mod/Overimagined.java
@@ -0,0 +1,148 @@
+package de.isuewo.overimagined_mod;
+
+import io.sigpipe.jbsdiff.InvalidHeaderException;
+import io.sigpipe.jbsdiff.ui.FileUI;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.compress.compressors.CompressorException;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.Objects;
+
+public class Overimagined implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        boolean isDev = FabricLoader.getInstance().isDevelopmentEnvironment();
+
+        System.out.println("Initializing Overimagined...");
+
+        String baseName = "ComplementaryReimagined_r2.0.1";
+        String baseDownloadUrl = "https://www.complementary.dev/reimagined/";
+        String baseTarHash = "39cb4dfd7c43b117a27e536f6ddc7c1e";
+        int baseTarSize = 768512;
+
+        String patchName = "OverimaginedShaders-2.0-Beta1";
+
+        File shaderpacks = FabricLoader.getInstance().getGameDir().resolve("shaderpacks").toFile();
+
+        File patchedFile = new File(shaderpacks, patchName);
+
+        if (!isDev && patchedFile.exists()) {
+            System.out.println(patchName + " is already installed.");
+            return;
+        }
+
+        File temp = Utils.createTempDir();
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                FileUtils.deleteDirectory(temp);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }));
+
+        File baseFile = new File(shaderpacks, baseName + ".zip");
+        File baseExtracted = new File(temp, baseName);
+
+        if (baseFile.exists()) {
+            Utils.extract(baseFile, baseExtracted);
+        } else { // in case the user has the source shaderpack already extracted
+            baseFile = new File(shaderpacks, baseName);
+            if (baseFile.exists()) {
+                baseExtracted = baseFile;
+            } else {
+                System.out.println(baseName + " is needed for " + patchName + ", but couldn't be found in your shaderpacks folder. Please download it from " + baseDownloadUrl + ", place it into your shaderpacks folder and restart Minecraft.");
+                return;
+            }
+        }
+
+        File baseArchived = new File(temp, baseName + ".tar");
+        Utils.archive(baseExtracted, baseArchived);
+
+        try {
+            if (isDev) {
+                String hash = DigestUtils.md5Hex(Files.newInputStream(baseArchived.toPath()));
+                System.out.println("Hash of " + baseName + ": " + hash);
+                System.out.println(baseArchived.length() + " bytes");
+            } else {
+                // needs to be done for compatibility with older minecraft version because they use an outdated version of commons-compress
+                byte[] fileBytes = Files.readAllBytes(baseArchived.toPath());
+                byte[] firstBytes = new byte[baseTarSize];
+                System.arraycopy(fileBytes, 0, firstBytes, 0, baseTarSize);
+
+                String hash = DigestUtils.md5Hex(firstBytes);
+                if (!hash.equals(baseTarHash)) {
+                    System.out.println("The version of " + baseName + " that was found in your shaderpacks can't be used as a base for " + patchName + ". Please download it again from " + baseDownloadUrl + ", place it into your shaderpacks folder and restart Minecraft.");
+                    return;
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("The version of " + baseName + " that was found in your shaderpacks can't be used as a base for " + patchName + ". Please download it again from " + baseDownloadUrl + ", place it into your shaderpacks folder and restart Minecraft.");
+            return;
+        }
+
+        File patchFile = new File(isDev ? shaderpacks : temp, patchName + ".patch");
+        File patchedArchive = new File(temp, patchName + ".tar");
+
+        if (isDev) {
+            Utils.archive(patchedFile, patchedArchive);
+            try {
+                FileUI.diff(baseArchived, patchedArchive, patchFile);
+            } catch (IOException | CompressorException | InvalidHeaderException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try (InputStream patchStream = getClass().getClassLoader().getResourceAsStream(patchName + ".patch")) {
+                try {
+                    FileUtils.copyInputStreamToFile(Objects.requireNonNull(patchStream), patchFile);
+                } catch (IOException e) {
+                    System.err.println("Failed to retrieve patch file." + e.getMessage());
+                    return;
+                }
+            } catch (IOException e) {
+                System.err.println("Failed to retrieve patch file." + e.getMessage());
+                return;
+            }
+
+            try {
+                FileUI.patch(baseArchived, patchedArchive, patchFile);
+            } catch (IOException | CompressorException | InvalidHeaderException e) {
+                System.err.println("Failed to apply patch." + e.getMessage());
+                return;
+            }
+
+            Utils.extract(patchedArchive, patchedFile);
+        }
+
+        System.out.println(patchName + " was successfully installed and is ready to nuke your GPU. Enjoy! -isuewo");
+
+        // Enable installed shaderpack by default
+        if (FabricLoader.getInstance().isModLoaded("iris")) {
+            File irisProperties = FabricLoader.getInstance().getGameDir().resolve("config/iris.properties").toFile();
+            try {
+                String irisPropertiesContent = FileUtils.readFileToString(irisProperties, "UTF-8");
+                irisPropertiesContent = irisPropertiesContent.replaceAll("enableShaders=false", "enableShaders=true").replaceAll("shaderPack=.*", "shaderPack=" + patchName);
+                FileUtils.writeStringToFile(irisProperties, irisPropertiesContent, "UTF-8");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else if (FabricLoader.getInstance().isModLoaded("optifabric")) {
+            File optionsshaders = FabricLoader.getInstance().getGameDir().resolve("optionsshaders.txt").toFile();
+            try {
+                String optionsshadersContent = FileUtils.readFileToString(optionsshaders, "UTF-8");
+                optionsshadersContent = optionsshadersContent.replaceAll("shaderPack=.+", "shaderPack=" + patchName);
+                FileUtils.writeStringToFile(optionsshaders, optionsshadersContent, "UTF-8");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("No supported shader-loader was found. Please install either Iris (recommended) or OptiFine.");
+        }
+    }
+}
---
+++ src/main/java/de/isuewo/overimagined_mod/Utils.java
@@ -0,0 +1,87 @@
+package de.isuewo.overimagined_mod;
+
+import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.ArchiveInputStream;
+import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
+import org.apache.commons.compress.utils.IOUtils;
+import org.apache.commons.io.FileUtils;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+public class Utils {
+
+    public static File createTempDir() {
+        return new File(FileUtils.getTempDirectoryPath() + File.separator + UUID.randomUUID());
+    }
+
+    public static void extract(File zipFile, File targetDir) {
+        try (ArchiveInputStream i = new ArchiveStreamFactory().createArchiveInputStream(new BufferedInputStream(Files.newInputStream(zipFile.toPath())))) {
+            ArchiveEntry entry;
+            while ((entry = i.getNextEntry()) != null) {
+                if (!i.canReadEntryData(entry)) {
+                    continue;
+                }
+                File f = new File(targetDir, entry.getName());
+                if (entry.isDirectory()) {
+                    if (!f.isDirectory() && !f.mkdirs()) {
+                        System.err.println("Failed to create directory " + f);
+                    }
+                } else {
+                    File parent = f.getParentFile();
+                    if (!parent.isDirectory() && !parent.mkdirs()) {
+                        System.err.println("Failed to create directory " + parent);
+                    }
+                    try (OutputStream o = Files.newOutputStream(f.toPath())) {
+                        IOUtils.copy(i, o);
+                    } catch (IOException e) {
+                        System.err.println("Failed to extract entry " + entry + " to " + f + ": " + e.getMessage());
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Failed to extract archive " + zipFile + ": " + e.getMessage());
+        }
+    }
+
+    public static void archive(File sourceDir, File archive) {
+        List<File> filesToArchive = new ArrayList<>(FileUtils.listFiles(sourceDir, null, true));
+
+        Collections.sort(filesToArchive); // ensures that the archive is deterministic
+
+        if (!archive.getParentFile().exists() && !archive.getParentFile().mkdirs()) {
+            System.err.println("Failed to create archive: " + archive);
+            return;
+        }
+
+        try (TarArchiveOutputStream o = new TarArchiveOutputStream(new BufferedOutputStream(Files.newOutputStream(archive.toPath())))) {
+            for (File f : filesToArchive) {
+                TarArchiveEntry entry = new TarArchiveEntry(f, sourceDir.toPath().relativize(f.toPath()).toString());
+
+                // also ensures that the archive is deterministic
+                entry.setModTime(0);
+                entry.setIds(0, 0);
+                entry.setNames("", "");
+
+                o.putArchiveEntry(entry);
+                if (f.isFile()) {
+                    try (InputStream i = Files.newInputStream(f.toPath())) {
+                        IOUtils.copy(i, o);
+                    } catch (IOException e) {
+                        System.err.println("Failed to archive entry " + entry + " from " + f + ": " + e.getMessage());
+                    }
+                }
+                o.closeArchiveEntry();
+            }
+            o.finish();
+        } catch (Exception e) {
+            System.err.println("Failed to create archive: " + e.getMessage());
+        }
+    }
+}

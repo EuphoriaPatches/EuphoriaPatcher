7ec50c4d65a33a6617f21ed71446c655573393af
Isuewo <g0vr26u7@duck.com>
2024-06-05 23:29:35 +0200

    Fix lunar, png and file sorting

--- src/main/java/de/isuewo/euphoria_patcher/ArchiveUtils.java
+++ src/main/java/de/isuewo/euphoria_patcher/ArchiveUtils.java
@@ -8,23 +8,24 @@ import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
 import org.apache.commons.compress.utils.IOUtils;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Comparator;
 import java.util.stream.Stream;
 
 public class ArchiveUtils {
     public static void extract(Path in, Path out) {
-        try (ArchiveInputStream ai = new ArchiveStreamFactory().createArchiveInputStream(Files.newInputStream(in))) {
+        try (ArchiveInputStream ai = new ArchiveStreamFactory().createArchiveInputStream(new BufferedInputStream(Files.newInputStream(in)))) {
             ArchiveEntry entry;
             while ((entry = ai.getNextEntry()) != null) {
                 if (!ai.canReadEntryData(entry)) {
                     continue;
                 }
-                if (!entry.isDirectory()) {
-                    Path targetFilePath = out.resolve(entry.getName());
+                Path targetFilePath = out.resolve(entry.getName());
+                if (entry.isDirectory()) {
+                    Files.createDirectories(targetFilePath);
+                } else {
                     Files.createDirectories(targetFilePath.getParent());
                     try (OutputStream o = Files.newOutputStream(targetFilePath)) {
                         IOUtils.copy(ai, o);
@@ -39,21 +40,30 @@ public class ArchiveUtils {
     public static void archive(Path sourceDir, Path archive) {
         try (TarArchiveOutputStream o = new TarArchiveOutputStream(Files.newOutputStream(archive))) {
             try (Stream<Path> walk = Files.walk(sourceDir)) {
-                walk.filter(Files::isRegularFile).sorted().forEach(f -> {
-                    TarArchiveEntry entry = new TarArchiveEntry(f.toFile(), sourceDir.relativize(f).toString());
-                    entry.setModTime(0);
-                    entry.setIds(0, 0);
-                    entry.setNames("", "");
-                    try (InputStream i = Files.newInputStream(f)) {
-                        o.putArchiveEntry(entry);
-                        IOUtils.copy(i, o);
-                        o.closeArchiveEntry();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                });
-                o.finish();
+                walk.sorted(Comparator.comparing(Path::toUri)) // platform-independent order
+                        .forEach(f -> {
+                            String fileName = sourceDir.relativize(f).toString().replace(File.separatorChar, '/'); // fixes weird issues with Lunar client
+                            TarArchiveEntry entry = new TarArchiveEntry(f.toFile(), fileName);
+                            // flags for deterministic archives
+                            entry.setModTime(0);
+                            entry.setIds(0, 0);
+                            entry.setNames("", "");
+                            try {
+                                o.putArchiveEntry(entry);
+                                if (Files.isRegularFile(f)) {
+                                    try (InputStream i = Files.newInputStream(f)) {
+                                        IOUtils.copy(i, o);
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                                o.closeArchiveEntry();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        });
             }
+            o.finish();
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/java/de/isuewo/euphoria_patcher/EuphoriaPatcher.java
+++ src/main/java/de/isuewo/euphoria_patcher/EuphoriaPatcher.java
@@ -43,12 +43,13 @@ public class EuphoriaPatcher implements ModInitializer {
         final boolean isDev = FabricLoader.getInstance().isDevelopmentEnvironment();
 
         final Path shaderpacks = FabricLoader.getInstance().getGameDir().resolve("shaderpacks");
+        System.out.println("Shaderpacks folder: " + shaderpacks);
 
         final String downloadURL = "https://www.complementary.dev/";
         final String brandName = "ComplementaryShaders";
-        final String version = "_r5.1.1";
+        final String version = "_r5.2";
         final String patchName = "EuphoriaPatches";
-        final String patchVersion = "_1.2";
+        final String patchVersion = "_1.3";
         final String commonLocation = "shaders/lib/common.glsl";
 
         // Detect which version(s) of Complementary Shaders the user has installed
@@ -62,6 +63,7 @@ public class EuphoriaPatcher implements ModInitializer {
         })) {
             for (Path potentialFile : stream) {
                 String name = potentialFile.getFileName().toString();
+                System.out.println("Checking file for Complementary: " + name);
                 if (name.endsWith(".zip")) {
                     if (name.contains("Reimagined")) {
                         styleReimagined = true;
@@ -90,6 +92,7 @@ public class EuphoriaPatcher implements ModInitializer {
                 })) {
                     for (Path potentialFile : stream2) {
                         String name = potentialFile.getFileName().toString();
+                        System.out.println("Checking Directory for Complementary: " + name);
                         if (name.contains(patchName)) {
                             if(name.contains(patchName + patchVersion)) {
                                 isAlreadyInstalled = true;
@@ -123,16 +126,20 @@ public class EuphoriaPatcher implements ModInitializer {
             }
             return;
         }
+        System.out.println("BaseFile: " + baseFile);
 
         final Path temp;
         try {
             temp = Files.createTempDirectory("euphoria-patcher-");
+            System.out.println("Temp: " + temp);
         } catch (IOException e) {
             log(2, "Error creating temporary directory" + e.getMessage());
             return;
         }
         final String baseName = baseFile.getFileName().toString().replace(".zip", "");
+        System.out.println("BaseName: " + baseName);
         final String patchedName = baseName + " + " + patchName + patchVersion;
+        System.out.println("BaseName: " + patchedName);
 
         Path baseExtracted = temp.resolve(baseName);
         if (!Files.isDirectory(baseFile)) {
@@ -140,9 +147,11 @@ public class EuphoriaPatcher implements ModInitializer {
         } else {
             baseExtracted = baseFile;
         }
+        System.out.println("baseExtracted: " + baseExtracted);
 
         try {
             final Path commons = baseExtracted.resolve(commonLocation);
+            System.out.println("commons: " + baseExtracted);
             final String config = FileUtils.readFileToString(commons.toFile(), "UTF-8").replaceFirst("SHADER_STYLE [14]", "SHADER_STYLE 1");
             FileUtils.writeStringToFile(commons.toFile(), config, "UTF-8");
         } catch (IOException e) {
@@ -150,20 +159,25 @@ public class EuphoriaPatcher implements ModInitializer {
             return;
         }
 
-        final String baseTarHash = "b5493f3d688e26814a04c6b1708adeb0";
-        final int baseTarSize = 1134592;
+        final String baseTarHash = "2e8d709e172ef054ed2cba837e039e3e";
+        final int baseTarSize = 1255424;
         final Path baseArchived = temp.resolve(baseName + ".tar");
+        System.out.println("baseArchived:" + baseArchived);
         ArchiveUtils.archive(baseExtracted, baseArchived);
 
         final Path patchedFile = shaderpacks.resolve(patchedName);
+        System.out.println("patchedFile:" + patchedFile);
         try {
             if (isDev) {
                 String hash = DigestUtils.md5Hex(Files.newInputStream(baseArchived));
+
                 log(0, "Hash of " + baseName + ": " + hash);
                 log(0,FileUtils.sizeOf(baseArchived.toFile()) + " bytes");
             } else {
                 // for compatibility with older minecraft version because they use an outdated version of commons-compress
                 String hash = DigestUtils.md5Hex(Arrays.copyOf(Files.readAllBytes(baseArchived), baseTarSize));
+                System.out.println("hash:" + hash);
+                System.out.println("baseTarHash:" + baseTarHash);
                 if (!hash.equals(baseTarHash)) {
                     log(1, "The version of " + brandName + " that was found in your shaderpacks can't be used as a base for " + patchName + ". Please download " + brandName + version + " from " + downloadURL + ", place it into your shaderpacks folder and restart Minecraft.");
                     return;

75ede4a34c72d33966a1fb5acb266353e202b3ce
SpacEagle17 <46494907+SpacEagle17@users.noreply.github.com>
2024-08-26 18:46:05 +0200

    Update Config.java

--- src/main/java/mc/euphoria_patches/euphoria_patcher/Config.java
+++ src/main/java/mc/euphoria_patches/euphoria_patcher/Config.java
@@ -12,41 +12,45 @@ public class Config {
     public static void createConfig() {
         try {
             Files.createFile(CONFIG_PATH);
+            try (FileWriter writer = new FileWriter(String.valueOf(CONFIG_PATH), true)) {
+                writer.write("# This file stores configuration options for the Euphoria Patcher mod\n");
+                writer.write("# Thank you for using Euphoria Patches - SpacEagle17\n");
+            }
             EuphoriaPatcher.log(0, "Successfully created config file");
         } catch (IOException e) {
-            EuphoriaPatcher.log(3, "Error creating config file: " + e.getMessage());
+            EuphoriaPatcher.log(3,0, "Error creating config file: " + e.getMessage());
         }
     }
 
-    public static void writeConfig(String option, String value) {
+    public static void writeConfig(String option, String value, String description) {
         try {
             if (!Files.exists(CONFIG_PATH)) createConfig();
             loadProperties();
-
-            if(!properties.containsKey(option)) { // If the option doesn't exist, add it
-                properties.setProperty(option, value);
-                EuphoriaPatcher.log(0, "Successfully wrote to config file: " + option + "=" + value);
-            }
-            try (OutputStream out = Files.newOutputStream(CONFIG_PATH)) {
-                String initialConfig = "This file stores configuration options for the Euphoria Patcher mod\n"
-                        + "Thank you for using Euphoria Patches - SpacEagle17";
-                properties.store(out, initialConfig);
+            if(!properties.containsKey(option)) {
+                try (FileWriter writer = new FileWriter(String.valueOf(CONFIG_PATH), true)) {
+                    writer.write(description != null ? "\n# " + description + "\n" : "\n");
+                    writer.write(option + "=" + value + "\n");
+                    EuphoriaPatcher.log(0, "Successfully wrote to config file: " + option + "=" + value);
+                }
             }
         } catch (IOException e) {
-            EuphoriaPatcher.log(3, "Error writing to config file: " + e.getMessage());
+            EuphoriaPatcher.log(3,0, "Error writing to config file: " + e.getMessage());
         }
     }
 
+    public static String readWriteConfig(String optionName, String defaultValue, String description) {
+        writeConfig(optionName, defaultValue, description);
+        return properties.getProperty(optionName, defaultValue); // Provide defaultValue if the property is missing
+    }
     public static String readWriteConfig(String optionName, String defaultValue) {
-        writeConfig(optionName, defaultValue);
-        return properties.getProperty(optionName);
+        return readWriteConfig(optionName, defaultValue, null);
     }
 
     private static void loadProperties() {
         try (InputStream in = Files.newInputStream(CONFIG_PATH)) {
             properties.load(in);
         } catch (IOException e) {
-            EuphoriaPatcher.log(3, "Error loading properties: " + e.getMessage());
+            EuphoriaPatcher.log(3,0, "Error loading properties: " + e.getMessage());
         }
     }
 }

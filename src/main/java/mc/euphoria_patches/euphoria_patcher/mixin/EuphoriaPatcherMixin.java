fd87b6a7752f6fbcda0fcbf38ee811ca8611451e
SpacEagle17 <46494907+SpacEagle17@users.noreply.github.com>
2024-09-03 18:52:25 +0200

    Update EuphoriaPatcherMixin.java

--- src/main/java/mc/euphoria_patches/euphoria_patcher/mixin/EuphoriaPatcherMixin.java
+++ src/main/java/mc/euphoria_patches/euphoria_patcher/mixin/EuphoriaPatcherMixin.java
@@ -1,9 +1,8 @@
 package mc.euphoria_patches.euphoria_patcher.mixin;
 
 import mc.euphoria_patches.euphoria_patcher.EuphoriaPatcher;
+import mc.euphoria_patches.euphoria_patcher.util.ServerCheck;
 import net.minecraft.server.Bootstrap;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.fml.loading.FMLLoader;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,10 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class EuphoriaPatcherMixin {
     @Inject(method = "bootStrap", at = @At("HEAD"))
     private static void onBootstrap(CallbackInfo ci) {
-        if (FMLLoader.getDist() == Dist.DEDICATED_SERVER) {
-            System.err.println("[EuphoriaPatcher] The Euphoria Patcher Mod should not be loaded on a server! Disabling...");
-            return;
-        }
+        if(ServerCheck.check()) return;
         new EuphoriaPatcher();
     }
 }

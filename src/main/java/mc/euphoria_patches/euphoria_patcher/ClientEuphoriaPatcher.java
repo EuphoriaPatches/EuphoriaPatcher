0eab9ba25ba1498c600b07b58dc1d41efb908b44
SpacEagle17 <46494907+SpacEagle17@users.noreply.github.com>
2024-09-04 15:25:39 +0200

    Update ClientEuphoriaPatcher.java

--- src/main/java/mc/euphoria_patches/euphoria_patcher/ClientEuphoriaPatcher.java
+++ src/main/java/mc/euphoria_patches/euphoria_patcher/ClientEuphoriaPatcher.java
@@ -1,17 +1,13 @@
 package mc.euphoria_patches.euphoria_patcher;
 
-import net.minecraftforge.api.distmarker.Dist;
+import mc.euphoria_patches.euphoria_patcher.util.ServerCheck;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.loading.FMLEnvironment;
 
 @Mod("euphoria_patcher")
 public class ClientEuphoriaPatcher {
 
     public ClientEuphoriaPatcher() {
-        if(FMLEnvironment.dist == Dist.DEDICATED_SERVER) {
-            System.err.println("The Euphoria Patcher Mod should not be loaded on a server! Disabling...");
-            return;
-        }
+        if(ServerCheck.check()) return;
         new EuphoriaPatcher();
     }
 }

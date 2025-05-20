16ea6c82135c4a660c0a233b162faea5a5724412
SpacEagle17 <46494907+SpacEagle17@users.noreply.github.com>
2025-05-20 18:34:05 +0200

    a

--- src/main/java/mc/euphoria_patches/euphoria_patcher/EuphoriaPatcher.java
+++ src/main/java/mc/euphoria_patches/euphoria_patcher/EuphoriaPatcher.java
@@ -24,7 +24,7 @@ public class EuphoriaPatcher {
     public static final String BRAND_NAME = "Complementary";
     public static final String PATCH_NAME = "EuphoriaPatches";
     public static final String VERSION = "_r5.5.1";
-    public static final String PATCH_VERSION = "_1.6.2";
+    public static final String PATCH_VERSION = "_1.6.3";
 
     public static final int BASE_TAR_SIZE = 1340416;
 
